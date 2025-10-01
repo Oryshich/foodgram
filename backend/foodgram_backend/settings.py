cb7d8dba65f5784c1bf263d07b6106c51f1412e9
Евгений Орышич <egenor@mail.ru>
2025-10-01 21:55:58 +0300

    My 30 commit

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -126,7 +126,6 @@ STATIC_ROOT = BASE_DIR / 'collected_static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CSRF_TRUSTED_ORIGINS = ['https://mynewfoodgram.gotdns.ch']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
